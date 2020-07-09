29e0aadc3e432ef85765b6a97fa465e91263476d
Vera <sk70885@hotmail.com>
2020-07-09 23:37:02 +0800

    hw3

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,7 @@
 /* eslint-disable indent */
 /* eslint-disable no-unused-vars */
+/* eslint-disable consistent-return */
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -16,19 +18,19 @@ function solve(input) {
     const count = lines.length;
     function isPrime(n) {
         if (n === 1) {
-            console.log('Composite');
-        } else if (n > 1) {
+            return false;
+        }
+        if (n > 1) {
             for (let i = 2; i < n; i += 1) {
                 if (n % i === 0) {
-                    console.log('Composite');
-                    return;
+                    return false;
                 }
             }
-            console.log('Prime');
+            return true;
         }
     }
     for (let i = 1; i < count; i += 1) {
-        isPrime(Number(lines[i]));
+        console.log(isPrime(Number(lines[i])) ? 'Prime' : 'Composite');
     }
 }
 
